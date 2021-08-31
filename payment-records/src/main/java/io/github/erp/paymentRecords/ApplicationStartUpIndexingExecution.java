c2e7945e1f2bfae4ed7c5a1538a8c4a8011f6ba1
Edwin Njeru <mailnjeru@gmail.com>
2021-08-31 14:15:08 +0300

    Scaffold application launch scripts with monitoring

--- payment-records/src/main/java/io/github/erp/paymentRecords/ApplicationStartUpIndexingExecution.java
+++ payment-records/src/main/java/io/github/erp/paymentRecords/ApplicationStartUpIndexingExecution.java
@@ -2,13 +2,18 @@ package io.github.erp.paymentRecords;
 
 import io.github.erp.domain.Payment;
 import io.github.erp.repository.PaymentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ApplicationStartUpIndexingExecution implements ApplicationListener<ApplicationReadyEvent> {
 
+    private static final Logger log = LoggerFactory.getLogger(ApplicationStartUpIndexingExecution.class);
     private final PaymentRepository paymentRepository;
     private final ApplicationIndexingService<Payment> applicationIndexingService;
 
@@ -19,7 +24,12 @@ public class ApplicationStartUpIndexingExecution implements ApplicationListener<
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
+        List<Payment> storedEntities = paymentRepository.findAll();
 
-        applicationIndexingService.index(paymentRepository.findAll());
+        log.info("Initiating startup index hydration with {} entities", storedEntities.size());
+
+        applicationIndexingService.index(storedEntities);
+
+        log.info("Search index initiated and ready for queries...");
     }
 }
